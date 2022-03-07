40b4569c4f796a5c592f881fa763fa61ca83f6ff
Jared Forsyth <jared@jaredforsyth.com>
2022-03-07 16:47:23 -0600

    undo change

--- src/index.js
+++ src/index.js
@@ -12,8 +12,6 @@ import {generateResponseType} from './generateResponseType';
 import {generateVariablesType} from './generateVariablesType';
 import type {Config, Options, Schema} from './types';
 
-// A change
-
 const optionsToConfig = (
     schema: Schema,
     definitions: $ReadOnlyArray<DefinitionNode>,
