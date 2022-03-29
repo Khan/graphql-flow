9810bfef0d590036039106a5b86b2a244de42831
Jared Forsyth <jared@jaredforsyth.com>
2022-03-29 10:44:51 -0600

    Allow customization of the "regenerate command" that's in the file docstrings (#16)
    ## Summary:
    Doesn't really make sense for it to be hard-coded.
    
    Issue: 
    
    ## Test plan:
    Checks: ✅ Lint & Test (ubuntu-latest, 16.x)
    
    Author: jaredly
    
    Reviewers: kevinbarabash, benchristel, jeremywiebe
    
    Required Reviewers:
    
    Approved By: kevinbarabash
    
    Pull Request URL: https://github.com/Khan/graphql-flow/pull/16

--- src/types.js
+++ src/types.js
@@ -14,6 +14,7 @@ import type {
 export type Selections = $ReadOnlyArray<SelectionNode>;
 
 export type Options = {|
+    regenerateCommand?: string,
     strictNullability?: boolean, // default true
     readOnlyArray?: boolean, // default true
     scalars?: Scalars,
