7dc287eba91496e8eba58a04c22f43a54f48fcdb
Jared Forsyth <jared@jaredforsyth.com>
2022-04-06 09:41:25 -0500

    validate config

--- src/cli/config.js
+++ src/cli/config.js
@@ -15,17 +15,47 @@ export type CliConfig = {
  * object.
  */
 type JSONConfig = {
-    excludes: Array<string>,
+    excludes?: Array<string>,
     schemaFilePath: string,
-    options: ExternalOptions,
+    options?: ExternalOptions,
 };
 
 export const loadConfigFile = (configFile: string): CliConfig => {
     // eslint-disable-next-line flowtype-errors/uncovered
     const data: JSONConfig = JSON.parse(fs.readFileSync(configFile, 'utf8'));
+    const toplevelKeys = ['excludes', 'schemaFilePath', 'options'];
+    Object.keys(data).forEach((k) => {
+        if (!toplevelKeys.includes(k)) {
+            throw new Error(
+                `Invalid attribute in config file ${configFile}: ${k}. Allowed attributes: ${toplevelKeys.join(
+                    ', ',
+                )}`,
+            );
+        }
+    });
+    if (data.options) {
+        const externalOptionsKeys = [
+            'pragma',
+            'loosePragma',
+            'ignorePragma',
+            'scalars',
+            'strictNullability',
+            'regenerateCommand',
+            'readOnlyArray',
+        ];
+        Object.keys(data.options).forEach((k) => {
+            if (!externalOptionsKeys.includes(k)) {
+                throw new Error(
+                    `Invalid option in config file ${configFile}: ${k}. Allowed options: ${externalOptionsKeys.join(
+                        ', ',
+                    )}`,
+                );
+            }
+        });
+    }
     return {
-        options: data.options,
-        excludes: data.excludes.map((string) => new RegExp(string)),
+        options: data.options ?? {},
+        excludes: data.excludes?.map((string) => new RegExp(string)) ?? [],
         schemaFilePath: path.join(
             path.dirname(configFile),
             data.schemaFilePath,
